34bff78f6af6c0c798ecd7bf46184d0fdd576236
TemaThe <tema812@gmail.com>
2014-08-11 09:59:24 +0400

    add DB and models

--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import patterns, include, url
 from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
@@ -16,10 +16,12 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:ra
     # url(r'^admin/', include(admin.site.urls)),
     url(r'^rango/', include('rango.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+
 )
 
 if settings.DEBUG:
     urlpatterns += patterns('django.views.static',
-                            (r'media/(?P<path>.*'),
+                            (r'media/(?P<path>.*)',
                             'serve',
-                            {'document_root' : settings.MEDIA_ROOT}, )
+                            {'document_root': settings.MEDIA_ROOT}), )
